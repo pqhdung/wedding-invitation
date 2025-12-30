d61a2970b225c289b6bfecb659284aee1bda9feb
pqhdung <pqhdung@pqhdung-LG-kilograms.local>
2025-12-30 23:51:37 +0700

    fix animation

--- assets/js/invitation.js
+++ assets/js/invitation.js
@@ -29,7 +29,7 @@ document.getElementById('enterInvite')?.addEventListener('click', () => {
   setTimeout(() => {
     AOS.init({ 
       once: true,
-      duration: 1200,   // ⏱️ thời gian chạy (ms)
+      duration: 800,   // ⏱️ thời gian chạy (ms)
       easing: 'ease-in-out',
       offset: 120       
     });
